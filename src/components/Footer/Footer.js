3a8bf494e2c9d6d030df9f177b2fd3c556789767
vicentepaiva <vicentepaivadev@gmail.com>
2021-11-30 18:05:15 -0300

    First commi

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made by
+        Vicente Paiva
+      </div>
+    );
+  };
+  
+  export default Footer;
